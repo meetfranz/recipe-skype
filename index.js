834f99b51e4ca4a1755d7c94240a6c8b37a2e9f4
Michael <mderazon@gmail.com>
2018-02-15 21:46:15 +0200

    fake user agent to enable calls
    meetfranz/recipe-skype#1

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = Franz => class Skype extends Franz {
     'new-window': '_linkFix',
   }
 
+  overrideUserAgent() {
+    return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
+  }
+
   _linkFix(event) {
     console.log('link redirect', event);
     event.preventDefault();
