6e254991f71179e55ade0a7189a9a37f79feed74
Kelvin Au <kau@fortinet.com>
2018-11-19 14:24:07 -0800

    use BrowserWindow instead of window.open for using the same cookie skypetoken_asm

--- webview.js
+++ webview.js
@@ -1,5 +1,7 @@
 import path from 'path';
-import { shell } from 'electron';
+import { shell, remote } from 'electron';
+
+const { BrowserWindow } = remote;
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
@@ -19,7 +21,17 @@ module.exports = (Franz) => {
       e.stopImmediatePropagation();
       e.preventDefault();
 
-      window.open(elem.getAttribute('href'));
+      let win = new BrowserWindow({
+        width: 800,
+        height: window.innerHeight,
+        minWidth: 600,
+      });
+
+      win.loadURL(elem.getAttribute('href'));
+
+      win.on('closed', () => {
+        win = null
+      })
     }
   }, true);
 };
