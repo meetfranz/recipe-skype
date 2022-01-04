cbe333d53ee5ef1a33b19c9e1c65704be53bfe94
Stefan Malzner <stefan@adlk.io>
2022-01-04 15:52:45 +0100

    Add support for Franz 5.8.0

--- webview.js
+++ webview.js
@@ -1,8 +1,4 @@
-import { remote } from 'electron';
-
-const { BrowserWindow } = remote;
-
-module.exports = (Franz, settings) => {
+module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
     const container = document.querySelector('[role="tablist"] > [title="Chats"] > div');
@@ -23,39 +19,4 @@ module.exports = (Franz, settings) => {
   };
 
   Franz.loop(getMessages);
-
-  // Link fix
-  // The `franzVersion` param is available since 5.4.0-beta.2
-  if (settings.franzVersion) {
-    document.addEventListener('click', (event) => {
-      const link = event.target.closest('a[href^="http"]');
-      const button = event.target.closest('button[title^="http"]');
-
-      if (link || button) {
-        const url = link ? link.getAttribute('href') : button.getAttribute('title');
-        event.preventDefault();
-        event.stopPropagation();
-
-
-        if (url.includes('views/imgpsh_fullsize_anim')) {
-          let win = new BrowserWindow({
-            width: 800,
-            height: window.innerHeight,
-            minWidth: 600,
-            webPreferences: {
-              partition: `persist:service-${settings.id}`,
-            },
-          });
-
-          win.loadURL(url);
-
-          win.on('closed', () => {
-            win = null;
-          });
-        } else {
-          window.open(url);
-        }
-      }
-    }, true);
-  }
 };
